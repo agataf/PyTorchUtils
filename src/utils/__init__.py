de39527f350fa9532751812a200b571625726d32
Agata Foryciarz <agata@foryciarz.com>
2018-02-19 13:42:27 -0500

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,5 +5,5 @@ from sample_spec import SampleSpec
 from utils import make_required_dirs, log_tagged_modules
 from utils import create_network, load_network, load_learning_monitor
 from utils import save_chkpt, load_chkpt, iter_from_chkpt_fname
-from utils import masks_not_empty, make_variable, read_h5, write_h5
+from utils import masks_not_empty, make_variable, read_h5, write_h5, resize_image
 from utils import set_gpus
