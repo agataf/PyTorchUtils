9866156c8712d097114fbf184e4aa744e75045d3
Agata Foryciarz <agata@foryciarz.com>
2018-02-19 14:09:41 -0500

    Update utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -16,12 +16,14 @@ import h5py
 import numpy as np
 
 def resize_image(img, resize):
-    if len(img.shape) > 2:
-        img1 = np.array((img.shape[0], img.shape[1]/resize, img.shape[2]/resize))
-        for i in range(img.shape[0]):
-            img1[i,:,:] = misc.imresize(img[i,:,:], 1.0/resize, interp="bilinear")
-    else:
-        img = misc.imresize(img, 1.0/resize, interp="bilinear")
+    img1 = np.array((img.shape[0], img.shape[1]/resize, img.shape[2]/resize))
+    for i in range(img.shape[0]):
+        print("img1.shape", img1.shape)
+        resized = misc.imresize(img[i,:,:], 1.0/resize, interp="bilinear")
+        print("resized.shape", resized.shape)
+        img1[i,:,:] = misc.imresize(img[i,:,:], 1.0/resize, interp="bilinear")
+#     else:
+#         img = misc.imresize(img, 1.0/resize, interp="bilinear")
     return img
 
 
