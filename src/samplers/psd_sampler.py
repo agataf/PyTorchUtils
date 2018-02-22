c18daf25875120280554b0e69cd4c2903e2ab448
Agata Foryciarz <agata@foryciarz.com>
2018-02-22 17:04:35 -0500

    Update psd_sampler.py

--- src/samplers/psd_sampler.py
+++ src/samplers/psd_sampler.py
@@ -22,7 +22,7 @@ def read_file(fname):
 
 class Sampler(object):
 
-    def __init__(self, datadir, dsets=[], mode="train", patchsz=(18,160,160), resize=1):
+    def __init__(self, datadir, dsets=[], mode="train", patchsz=(18,320,320), resize=1):
 
       assert mode in ["train","val","test"]
 
