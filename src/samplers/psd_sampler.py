a4e4d26f96693f3416b2755c34681ad754d53e32
Agata Foryciarz <agata@foryciarz.com>
2018-03-04 16:35:48 -0500

    Update psd_sampler.py

--- src/samplers/psd_sampler.py
+++ src/samplers/psd_sampler.py
@@ -45,7 +45,7 @@ class Sampler(object):
       for dset in dsets:
         vdp.add_dataset( self.build_dataset(datadir, spec, dset, resize) )
       if mode == "train":
-          vdp.set_sampling_weights([0.5, 0.1, 0.1, 0.1, 0.1, 0.025, 0.025, 0.025, 0.025])
+          vdp.set_sampling_weights([0.5, 0.1, 0.025, 0.1, 0.1, 0.025, 0.025, 0.025, 0.1])
       if mode == "val":
           vdp.set_sampling_weights([0.5, 0.1, 0.1, 0.1, 0.1])
       vdp.set_augmentor(self._aug(mode))
