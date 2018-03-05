8bcbd4ae5180b966a2459c26105bcacc83a879eb
Agata Foryciarz <agata@foryciarz.com>
2018-03-04 21:31:21 -0500

    Update run_exp.py

--- src/run_exp.py
+++ src/run_exp.py
@@ -59,8 +59,8 @@ def fill_params(expt_name, chkpt_num, batch_sz, lr, gpus,
     #Sampling params
     params["data_dir"]     = os.path.expanduser("~/seungmount/research/agataf/datasets/pinky_all")
     assert os.path.isdir(params["data_dir"]),"nonexistent data directory"
-    train_vol_list = ["vol19-34_train",  "vol101_train", "vol102_train", "vol103_train", "vol104_train", "vol401_train", "vol501_train", "vol502_train", "vol503_train"]
-    val_vol_list = ["vol19-34_val", "vol401_val", "vol501_val", "vol502_val", "vol503_val"]
+    train_vol_list = ["vol201_train", "vol401_train"]
+    val_vol_list = ["vol201_train", "vol401_train"]
     if erode:
         params["train_sets"]   = [el+"_3eroded" for el in train_vol_list]
         params["val_sets"]   = [el+"_3eroded" for el in val_vol_list]
