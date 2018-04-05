381015663d7570d692649e660f12ca07454a57d1
Agata Foryciarz <agata@foryciarz.com>
2018-04-05 18:12:40 -0400

    Update manual_inspect_net.py

--- src/manual_inspect_net.py
+++ src/manual_inspect_net.py
@@ -6,11 +6,11 @@ def write_h5(data,fname):
   with h5py.File(fname,"w") as f:                          
     f.create_dataset("/main",data=data)    
  
- params = fill_params('exp8', 430000, ['0'], True, 'models/RSUNet_down_interm.py', '', '')
- d=torch.load('experiments/exp8/models/model430000.chkpt') 
- params['model_kwargs']['bn']=False   
+params = fill_params('exp8', 430000, ['0'], True, 'models/RSUNet_down_interm.py', '', '')
+d=torch.load('experiments/exp8/models/model430000.chkpt') 
+params['model_kwargs']['bn']=False   
  
-  net = utils.create_network(**params)  
+net = utils.create_network(**params)  
 img = utils.read_h5('/usr/people/agataf/seungmount/research/agataf/datasets/pinky_all/chunk_19585-21632_22657-24704_4003-4258.omni_img.h5') 
 img=img[:16,:320,:320]    
 img = img.reshape((1,1) + img.shape)  
@@ -21,10 +21,22 @@ inputconv=net.module.inputconv(net.module.downsample(v))
 convmod0_conv1 = net.module.convmod0.conv1(inputconv)   
 convmod0_conv1_bn = net.module.convmod0.bn1(convmod0_conv1)   
 convmod0_conv1_act = net.module.convmod0.activation(convmod0_conv1_bn)
-ret1 = convmod0_conv1_act.data.cpu().numpy()  
+ret1 = convmod0_conv1_act.data.cpu().numpy()[0,0,...]
  
 write_h5(ret1,"ret1.h5") 
 
 convmod0_conv2 = net.module.convmod0.conv2(convmod0_conv1_act)   
 convmod0_conv2_bn = net.module.convmod0.bn2(convmod0_conv2)   
 convmod0_conv2_act = net.module.convmod0.activation(convmod0_conv2_bn)
+ret2 = convmod0_conv2_act.data.cpu().numpy()[0,0,...] 
+write_h5(ret2,"ret2.h5") 
+
+convmod0_conv3 = net.module.convmod0.conv3(convmod0_conv2_act)   
+ret2_5 = convmod0_conv3.data.cpu().numpy()[0,0,...] 
+write_h5(ret2_5,"ret_conv2_d0.h5") 
+
+convmod0_conv3_sum = convmod0_conv3+convmod0_conv1_act
+convmod0_conv3_bn = net.module.convmod0.bn3(convmod0_conv3_sum) 
+convmod0_conv3_act = net.module.convmod0.activation(convmod0_conv3_bn)
+ret3 = convmod0_conv3_act.data.cpu().numpy()[0,0,...] 
+write_h5(ret3,"ret_sum0_d0.h5") 
